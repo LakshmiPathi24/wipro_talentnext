11ed423b546eda5701cf8c37889a6084a2fe8671
LakshmiPathi24 <lakshmipathi8080@gmail.com>
2025-07-07 10:25:08 +0530

    JavaFundamentals_Arrays

---
+++ java_fundamentals/src/java_fundamentals/Arrays_4.java
@@ -0,0 +1,13 @@
+package java_fundamentals;
+import java.util.*;
+public class Arrays_4{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int[]a= {65,68,69};
+     for(int i:a) {
+    	 System.out.print((char)i+" ");
+     }
+	}
+
+}
